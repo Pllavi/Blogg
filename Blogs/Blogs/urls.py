dc9a28f1cf82af3744cf29606a2248fa93845b51
Aniket Lad <76205645+AniketAI@users.noreply.github.com>
2024-03-15 01:28:22 +0530

    Added new

--- Blogs/Blogs/urls.py
+++ Blogs/Blogs/urls.py
@@ -2,7 +2,7 @@
 URL configuration for Blogs project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,27 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from myblog import views
-
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('myblog.urls')),
-path('',views.SignupPage,name='signup'),
-    path('login/',views.LoginPage,name='login'),
-    path('home/',views.HomePage,name='home'),
-    path('logout/',views.LogoutPage,name='logout'),
-    path('dashboard/', views.HomePage, name='dashboard'),
-path('blog/', views.blog, name='blog'),
-path('subscription/', views.subscription_page, name='subscription_page'),
-                  # path('login/', views.CustomLoginView.as_view(), name='login'),
-                  path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-path('create/', views.create_blog_post, name='create_blog_post'),
-                  path('edit/<int:category_id>/<int:post_id>/', views.edit, name='edit'),
-
-
-
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('blogapp.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
